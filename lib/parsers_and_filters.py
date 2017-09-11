1614d4fb7fecb030f6845d3f4902f2ba5ab1234a
Allan L. R. Hansen <allan.lrh@gmail.com>
2017-09-11 18:03:49 +0200

    Added preliminary function which can update settings file.

--- lib/parsers_and_filters.py
+++ lib/parsers_and_filters.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-import barely_json
+import jsoncfg
 import os
+import json
 
 
 def load_setingsfile(filepath):
@@ -25,7 +26,15 @@ def load_setingsfile(filepath):
     """
     if not os.path.isfile(filepath):
         raise FileNotFoundError("The settings file {} could not be found".format(filepath))
-    with open(filepath) as fid:
-        filecontent = fid.read()
-    settings = barely_json.parse(filecontent)
+    settings = jsoncfg.load(filepath)
+    return settings
+
+
+def replace_params(settingsfile, rulesfile):
+    """Have a settings file on each system which are to overwrite the default settings on
+    that system.
+    """
+    settings = load_setingsfile(settingsfile)
+    rules = load_setingsfile(rulesfile)
+    settings.update(rules)
     return settings
